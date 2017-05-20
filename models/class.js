887f43f61969000024ac798b0537959441de6163
Cynicalbird <alex.ng.personal@gmail.com>
2017-05-20 15:36:35 -0400

    class model now checks if database is empty and loads a template stored in json file (classes.json)

--- models/class.js
+++ models/class.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var fs = require('fs');
 
 var classSchema = mongoose.Schema({
     classes: []
@@ -6,6 +7,40 @@ var classSchema = mongoose.Schema({
 
 var Class = module.exports = mongoose.model('Class', classSchema);
 
+//Ensure classes are loaded to database
+Class.findOne({}, function(error, doc){
+    //error loading classes from database
+    if (error)
+        console.log(error);
+    else
+    {
+        //if database is empty
+        if (!doc)
+        {
+            //read the game classes file
+            fs.readFile('templates/classes.json', 'utf-8', function(error, data){
+                if (error)
+                    return console.log(error);
+                //store to db
+                else
+                {
+                    var file = JSON.parse(data)
+                    var classes = new Class({
+                        classes: file.classes
+                    });
+                    console.log(classes);
+                    classes.save(function (error, classes){
+                        if (error)
+                            throw error;
+                        else
+                            console.log(classes);
+                    });
+                }
+            });
+        }
+    }
+});
+
 module.exports.getClasses = function (callback) {
-    Class.findOne({},callback);
+    Class.findOne({}, callback);
 }
